ecd8e5b12b41bffe97fd62968f0a8f73462684a4
juandpt03 <juandpt03@gmail.com>
2024-07-01 00:12:00 -0500

    chore: dependencies were imported

---
+++ README.md
@@ -0,0 +1,3 @@
+# fast_recipe
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/fast_recipe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fast_recipe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
